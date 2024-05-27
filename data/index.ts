981cd0b8fed2227851714f072fce4a5e4521cd46
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2024-05-27 14:48:22 +0200

    Added experience section

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,7 @@
 export const navItems = [
     { name: "About", link: "#about" },
     { name: "Projects", link: "#projects" },
-    { name: "Testimonials", link: "#testimonials" },
+    { name: "Experience", link: "#experience" },
     { name: "Contact", link: "#contact" },
   ];
   
@@ -157,29 +157,29 @@ export const navItems = [
   export const workExperience = [
     {
       id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      title: "M.Sc. in Software Design",
+      desc: "IT-university of Copenhagen. Applied Computer Science.",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
     {
       id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      title: "Creative Agency",
+      desc: "Founder and creator of campaigns with a socially responsible focus",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp2.svg",
     },
     {
       id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      title: "Physiotherapist",
+      desc: "Broad experience within the healthcare system and private clinics",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp3.svg",
     },
     {
       id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      title: "SoMe-manager & Videographer",
+      desc: "Managed social media video content for different companies",
       className: "md:col-span-2",
       thumbnail: "/exp4.svg",
     },
