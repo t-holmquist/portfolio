b0b694029ab73df1f30577106ad74024cc3ce64b
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2024-05-28 13:41:14 +0200

    Changed styling of 3d-pins

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -21,7 +21,7 @@ export const PinContainer = ({
   );
 
   const onMouseEnter = () => {
-    setTransform("translate(-50%,-50%) rotateX(40deg) scale(0.8)");
+    setTransform("translate(-50%,-50%) rotateX(0deg) scale(0.9)");
   };
   const onMouseLeave = () => {
     setTransform("translate(-50%,-50%) rotateX(0deg) scale(1)");
@@ -92,7 +92,7 @@ export const PinPerspective = ({
         <div
           style={{
             perspective: "1000px",
-            transform: "rotateX(70deg) translateZ(0)",
+            transform: "rotateX(20deg) translateZ(0) scale(2)",
           }}
           className="absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2"
         >
