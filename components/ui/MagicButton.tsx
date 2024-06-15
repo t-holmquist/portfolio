52325739cf74e9607245eda29cb54a985a895119
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2024-06-15 20:04:25 +0200

    New button styling and default nav behavior

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -4,8 +4,7 @@ const MagicButton = ({
   title: string, icon: React.ReactNode, position: string, handleClick?: () => void; otherClasses?: string; 
 }) => {
   return (
-    <button onClick={handleClick} className="w-60 relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none">
-        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+    <button onClick={handleClick} className="border-slate-200 border-[1px] w-60 relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none">  
         <span className={`${otherClasses} inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2`}>
             {position === 'left' && icon}
             {title}
