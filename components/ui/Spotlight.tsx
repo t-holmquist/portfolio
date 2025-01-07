992421c002eac9ee94e84f6f10c6bbd676022a48
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-01-07 14:26:24 +0100

    new spotlight shape

--- components/ui/Spotlight.tsx
+++ components/ui/Spotlight.tsx
@@ -21,7 +21,7 @@ export const Spotlight = ({ className, fill }: SpotlightProps) => {
           cx="1924.71"
           cy="273.501"
           rx="1924.71"
-          ry="273.501"
+          ry="473.501"
           transform="matrix(-0.822377 -0.568943 -0.568943 0.822377 3631.88 2291.09)"
           fill={fill || "white"}
           fillOpacity="0.21"
