9fd11e10454265232b6a673dfaf8d84b7303f17a
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-10-04 22:05:17 +0200

    Update current employment indicator design

--- components/currentemployment.tsx
+++ components/currentemployment.tsx
@@ -1,9 +1,35 @@
+'use client'
+
 import { skillsData } from "@/data"
 import GlareButton from "./ui/glarebutton"
 import ActiveIndicator from "./ui/activeIndicator"
 import Image from "next/image"
+import { motion, useInView } from "motion/react"
+import { useRef } from "react"
 
 const CurrentEmployment = () => {
+
+
+    const parentVariant = {
+        visible: {
+            transition: {
+                staggerChildren: 0.3,
+            }
+        },
+        hidden: {}
+    };
+
+    const childVariant = {
+        visible: {opacity: 1, y: 0},
+        hidden: {opacity: 0, y: 20}
+    }
+
+    // Hook for checking if element is in view and then trigger animation exactly at that point.
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true, amount: 0.5, });
+
+
+
     return (
         <section className="relative mt-8 md:mt-14 bg-gradient-to-r from-brandSecondary/10 to-brandAccent/10 border border-brandBorder rounded-2xl">
             {/* Dots bg */}
@@ -11,20 +37,31 @@ const CurrentEmployment = () => {
             <div className="lg:flex-row flex flex-col p-5 sm:p-8 gap-10 lg:gap-0">
                 {/* Text and skills */}
                 <div className="order-last lg:order-first flex flex-col gap-8">
-                    <div className="flex gap-2 items-center">
+                    <div className="flex gap-2 items-center bg-[#30412d] w-fit p-2 rounded-sm">
                         <ActiveIndicator />
-                        <p className="font-sourceSans text-sm text-brandSecondary">Current employment</p>
+                        <p className="font-sourceSans text-[#9bec6c] text-sm">Current employment</p>
                     </div>
-                    <div className="space-y-3">
-                        <h3 className="font-oswald text-2xl font-bold">Center for IT og Medicoteknologi</h3>
-                        <p className="font-sourceSans text-sm text-brandSecondary">Region Hovedstaden</p>
-                        <p className="font-sourceSans sm:text-lg max-w-xl">
+                    <motion.div
+                    ref={ref} 
+                    variants={parentVariant}
+                    initial="hidden"
+                    animate={isInView && 'visible'}
+                    className="space-y-3">
+                        <motion.h3
+                        variants={childVariant} 
+                        className="font-oswald text-2xl font-bold">Center for IT og Medicoteknologi</motion.h3>
+                        <motion.p 
+                        variants={childVariant} 
+                        className="font-sourceSans text-sm text-brandSecondary">Region Hovedstaden</motion.p>
+                        <motion.p 
+                        variants={childVariant} 
+                        className="font-sourceSans sm:text-lg max-w-xl">
                             I currently work as a Student Assistant, where I focus on enhancing the user experience of Min Sundhedsplatform (the patient health platform) and conduct application testing for Sundhedsplatformen for clinicians in Region Hovedstaden.
                             <br/>
                             Recently, I improved the UX of Min Sundhedsplatform by using patient feedback to update the menu navigation, making it easier for patients to find critical medical information.
                             Our team operates within the SAFe agile framework, which has provided me with valuable experience in planning and executing tasks efficiently in a fast-paced environment. I have been entrusted with significant responsibility, allowing me to leverage my technical skills to prototype and improve products used by millions of patients and clinicians.
-                        </p>
-                    </div>
+                        </motion.p>
+                    </motion.div>
                     <div>
                         <h4 className="font-oswald text-lg font-bold">Skills that I am building in this job</h4>
                         {/* Skills */}
@@ -36,10 +73,14 @@ const CurrentEmployment = () => {
                     </div>
                 </div>
                 {/* image */}
-                <aside className="relative h-fit w-full min-w-[48%]">
+                <motion.aside 
+                initial={{y: 10}}
+                animate={{y: 0}}
+                transition={{repeatType: 'reverse', repeat: Infinity, duration: 5, ease: 'easeInOut'}}
+                className="relative h-fit w-full min-w-[48%]">
                     <Image className="rounded-2xl border border-brandBorder" width={620} height={600} alt="team day image" src={"/cimt.webp"}/>
                     <div className="absolute font-sourceSans text-xs sm:text-sm -bottom-2 left-6 bg-brandPrimary rounded-full py-1 px-2 border border-white">Team day</div>
-                </aside>
+                </motion.aside>
             </div>
         </section>
     )
--- components/ui/activeIndicator.tsx
+++ components/ui/activeIndicator.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const ActiveIndicator = () => {
   return (
-    <motion.div className="relative w-4 h-4 bg-green-400 rounded-full">
+    <motion.div className="relative w-3 h-3 bg-green-400 rounded-full">
         {/* this wrapper div centers the motion divs to avoid origin transform when applying animation -> shifting the circles position */}
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <motion.div
@@ -18,7 +18,7 @@ const ActiveIndicator = () => {
                 repeat: Infinity,
                 repeatType: 'reverse',
             }}
-            className="w-8 h-8 bg-gray-100 border border-brandBorder rounded-full opacity-10"
+            className="w-7 h-7 bg-gray-100 border border-brandBorder rounded-full opacity-10"
             />
         </div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
@@ -31,7 +31,7 @@ const ActiveIndicator = () => {
                 repeat: Infinity,
                 repeatType: 'reverse',
             }}
-            className="w-6 h-6 bg-gray-200 rounded-full opacity-20"
+            className="w-5 h-5 bg-gray-200 rounded-full opacity-20"
             />
         </div>
 </motion.div>
